3c219dcdc75e4376b95390bf3da208f633c3f4cc
Andreas George Nechita <ga.nechita@gmail.com>
2024-04-24 17:52:30 +0200

    feat(theme): added unstyled navbar

--- app/public/wp-content/themes/customtheme/functions.php
+++ app/public/wp-content/themes/customtheme/functions.php
@@ -4,3 +4,9 @@ function my_theme_enqueue_styles() {
   wp_enqueue_style( 'my_theme_style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function activate_appearance_menu(){
+  register_nav_menu('header_menu','Header Menu Location');
+}
+
+add_action( 'after_setup_theme', 'activate_appearance_menu' );
